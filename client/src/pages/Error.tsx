723c3e65464664fb19b45e0fe29215aab158aa93
Bakodev <musaabdulkabeer19@gmail.com>
2023-11-13 18:19:29 +0100

    routes error

--- client/src/pages/Error.tsx
+++ client/src/pages/Error.tsx
@@ -1,10 +1,4 @@
-import { useRouteError } from 'react-router-dom';
-
 const Error: React.FC = () => {
-     const error: unknown = useRouteError();
-
-     if (error?.status === '404') return <h1>Not Found</h1>;
-
      return <div>Something went wrong</div>;
 };
 
